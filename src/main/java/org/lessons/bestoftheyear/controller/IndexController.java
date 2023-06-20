14bc6f068ac4303227e9a4adf5d1ec1a6aacf5df
marcantonio99 <marcantonio8799@gmail.com>
2023-06-20 16:56:18 +0200

    Step 1

--- src/main/java/org/lessons/bestoftheyear/controller/IndexController.java
+++ src/main/java/org/lessons/bestoftheyear/controller/IndexController.java
@@ -3,11 +3,10 @@ package org.lessons.bestoftheyear.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class indexController {
- @GetMapping("/template")
+public class IndexController {
+ @GetMapping("/")
  public String template(Model model){
   String name = "Marco";
   model.addAttribute("name", name);
