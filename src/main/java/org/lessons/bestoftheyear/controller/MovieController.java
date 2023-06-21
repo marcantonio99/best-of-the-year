e28c20476c65d4d796ca5402f809a1e9d4201de3
marcantonio99 <marcantonio8799@gmail.com>
2023-06-21 18:13:29 +0200

    song and movie return objects and not a string

--- src/main/java/org/lessons/bestoftheyear/controller/MovieController.java
+++ src/main/java/org/lessons/bestoftheyear/controller/MovieController.java
@@ -4,35 +4,75 @@ import org.lessons.bestoftheyear.model.Movie;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/movies")
 public class MovieController {
 
     @GetMapping
-    public String movieList(Model model){
-        model.addAttribute("movies", getMoviesString());
-        return "movie";
+    public String movieList(Model model, @RequestParam("search") Optional<String> searchString){
+        String movieString;
+        List<Movie> movieList;
+        if (searchString.isPresent()){
+            movieString = getMoviesString(searchString.get());
+            movieList = getBestMovies(searchString.get());
+        }else {
+            movieString = getMoviesString();
+            movieList = getBestMovies();
+        }
+        model.addAttribute("movies", movieString);
+        model.addAttribute("movieList", movieList);
+        return "movies";
+    }
+
+    @GetMapping("/list")
+    public String redirectList(){
+        return "redirect:/movies";
     }
 
     private List<Movie> getBestMovies() {
         List<Movie> movies = new ArrayList<>();
-                movies.add(new Movie(1, "Titolo del film 1"));
-                movies.add(new Movie(2, "Titolo del film 2"));
-                movies.add(new Movie(3, "Titolo del film 3"));
+           movies.add(new Movie(1, "Titolo del film 1"));
+           movies.add(new Movie(2, "Titolo del film 2"));
+           movies.add(new Movie(3, "Titolo del film 3"));
            return movies;
+    }
+
+    private List<Movie> getBestMovies(String search){
+        List<Movie> movies = getBestMovies();
+        List<Movie> result = new ArrayList<>();
+        for (Movie movie : movies){
+            if (movie.getTitle().toLowerCase().contains(search.toLowerCase())){
+                result.add(movie);
+            }
+        }
+        return result;
     }
 
     private String getMoviesString(){
         List<Movie> movieList = getBestMovies();
+        return concatList(movieList);
+    }
+
+    private static String concatList(List<Movie> movieList) {
         String concat = "";
         for (Movie movie : movieList){
             concat += movie.getTitle();
         }
-        return concat.substring(0, concat.length()-2);
+        if (concat.length() > 0){
+            return concat.substring(0, concat.length() - 2);
+        }else return "";
+    }
+
+    private String getMoviesString(String search){
+        List<Movie> movieList = getBestMovies(search);
+        return concatList(movieList);
     }
 }
--- src/main/java/org/lessons/bestoftheyear/controller/SongController.java
+++ src/main/java/org/lessons/bestoftheyear/controller/SongController.java
@@ -5,17 +5,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/songs")
 public class SongController {
     @GetMapping
-    public String movieList(Model model){
-        model.addAttribute("songs", getSongsString());
-        return "song";
+    public String songList(Model model, @RequestParam("search") Optional<String> searchString){
+        String songString;
+        List<Song> songList;
+        if (searchString.isPresent()){
+            songString = getSongsString(searchString.get());
+            songList = getBestSongs(searchString.get());
+        }else {
+            songString = getSongsString();
+            songList = getBestSongs();
+        }
+        model.addAttribute("songs", songString);
+        model.addAttribute("songList", songList);
+        return "songs";
+    }
+
+    @GetMapping("/list")
+    public String redirectList(){
+        return "redirect:/songs";
     }
 
     private List<Song> getBestSongs() {
@@ -26,12 +43,34 @@ public class SongController {
         return songs;
     }
 
+    private List<Song> getBestSongs(String search){
+        List<Song> songs = getBestSongs();
+        List<Song> result = new ArrayList<>();
+        for (Song song : songs){
+            if (song.getTitle().toLowerCase().contains(search.toLowerCase())){
+                result.add(song);
+            }
+        }
+        return result;
+    }
+
     private String getSongsString(){
         List<Song> songList = getBestSongs();
+        return concatList(songList);
+    }
+
+    private static String concatList(List<Song> songList) {
         String concat = "";
         for (Song song : songList){
             concat += song.getTitle();
         }
-        return concat.substring(0, concat.length()-2);
+        if (concat.length() > 0){
+            return concat.substring(0, concat.length() - 2);
+        }else return "";
+    }
+
+    private String getSongsString(String search){
+        List<Song> songList = getBestSongs(search);
+        return concatList(songList);
     }
 }
